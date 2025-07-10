4cfc6147c72e24c820b18fb8792c3d23642111a0
Wyatt Raimer <wyattraimer@gmail.com>
2025-07-10 15:49:23 -0600

    getting header setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Header></Header>
         <Routes>
-          <Route path='/' Component={Home}/>
+          <Route path='/' exact Component={Home}/>
           <Route path='/movie/:imdbID' Component={MovieDetail}/>
           <Route Component={PageNotFound}/>
         </Routes>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import user from "../../images/user.png";
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='header'>
+            <Link to='/'>
+                <div className='logo'>
+                    Movie App
+                </div>
+            </Link>
+            <div className='user-image'>
+                <img src={user} alt='user' />
+            </div>
         </div>
     );
 };
