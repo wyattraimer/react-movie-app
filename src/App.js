fbafe52eb8c41931799907fd68b4601a580ba0cb
Wyatt Raimer <wyattraimer@gmail.com>
2025-07-10 15:15:05 -0600

    setting up main app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Router>
+        <Header></Header>
+        <Route path='/' Component={Home}/>
+        <Route path='/movie/:imdbID' Component={MovieDetail}/>
+        <Route Component={PageNotFound}/>
+        <Footer />
+      </Router>
     </div>
   );
 }
