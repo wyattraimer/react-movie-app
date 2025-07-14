ffd884e5c210f0c5ee80b99ba81760404df90eb0
Wyatt Raimer <wyattraimer@gmail.com>
2025-07-14 11:38:58 -0600

    import and implement useDispatch and addMovies slice

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,14 @@ import React, { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing';
 import movieApi from '../../common/apis/movieApi';
 import { APIKey } from '../../common/apis/MovieApiKey';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 
 const Home = () => {
-
+  const movieText = "Harry";
+  const dispatch = useDispatch();
+  
   useEffect(() => {
-    const movieText = "Harry";
     const fetchMovies = async () => {
       const response = await movieApi.get(
         `?apiKey=${APIKey}&s=${movieText}&type=movie`
@@ -14,7 +17,7 @@ const Home = () => {
       .catch((err) => {
         console.log("Error: ", err);
       });
-      console.log("API response: ", response);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
   }, []);
