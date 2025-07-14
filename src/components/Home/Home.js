c5ff5e1e32fe1e5b801d96ee03170416bd52aa77
Wyatt Raimer <wyattraimer@gmail.com>
2025-07-14 11:11:38 -0600

    working on api and handling response

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import MovieListing from '../MovieListing/MovieListing';
+import movieApi from '../../common/apis/movieApi';
+import { APIKey } from '../../common/apis/MovieApiKey';
 
 const Home = () => {
+
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi.get(
+        `?apiKey=${APIKey}&s=${movieText}&type=movie`
+      )
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+      console.log("API response: ", response);
+    }
+  })
     return (
-        <div>
-          Home  
-        </div>
+      <div>
+        <div className='banner-img'></div>
+        <MovieListing />
+      </div>
     );
 };
 
