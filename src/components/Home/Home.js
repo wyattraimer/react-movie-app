ddc7e4896a32946d824247238237537a5062482f
Wyatt Raimer <wyattraimer@gmail.com>
2025-07-14 16:19:16 -0600

    move api response and put in thunk

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,14 @@
 import React, { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing';
-import movieApi from '../../common/apis/movieApi';
-import { APIKey } from '../../common/apis/MovieApiKey';
 import { useDispatch } from 'react-redux';
 import { addMovies } from '../../features/movies/movieSlice';
 
 const Home = () => {
-  const movieText = "Harry";
   const dispatch = useDispatch();
   
   useEffect(() => {
     const fetchMovies = async () => {
-      const response = await movieApi.get(
-        `?apiKey=${APIKey}&s=${movieText}&type=movie`
-      )
-      .catch((err) => {
-        console.log("Error: ", err);
-      });
-      dispatch(addMovies(response.data));
+      
     };
     fetchMovies();
   }, []);
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from '../../common/apis/movieApi';
+import { APIKey } from '../../common/apis/MovieApiKey';
+
+const fetchAsyncMovies = createAsyncThunk('movies/fetchAsyncMovies', async () => {
+    const movieText = "Harry";
+    const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+      return response.data;
+})
 
 const initialState = {
     movies: {},
