4ea25b5185f0c1b94b0e9b9e32ab581994a3652d
Wyatt Raimer <wyattraimer@gmail.com>
2025-07-14 12:50:02 -0600

    fixed error so api call shows in console

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getAllMovies } from '../../features/movies/movieSlice';
 
 const MovieListing = () => {
+    const movies = useSelector(getAllMovies);
+    console.log(movies);
     return (
         <div>
             MovieListing
