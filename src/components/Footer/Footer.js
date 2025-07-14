1caf638320d69773aedbeb7c158a1ad0a6be1182
Wyatt Raimer <wyattraimer@gmail.com>
2025-07-14 10:58:44 -0600

    Add footer to app. Add styles to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import "./Footer.scss";
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className='footer'>
+            <div>Movie App</div>
+            <div>©2025, Big R Movie, Inc. or its affiliates</div>
         </div>
     );
 };
