682954b52c6bf9168db4d26244c0119fe31f44dd
Wyatt Raimer <wyattraimer@gmail.com>
2025-07-15 11:17:48 -0600

    editing setting for carousel

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Slider from 'react-slick';
 import { useSelector } from 'react-redux';
 import { getAllMovies, getAllShows } from '../../features/movies/movieSlice';
 import MovieCard from '../MovieCard/MovieCard';
 import "./MovieListing.scss";
 
 const MovieListing = () => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 3,
+    };
     const movies = useSelector(getAllMovies);
     const shows = useSelector(getAllShows);
     let renderMovies, renderShows = "";
@@ -36,7 +44,9 @@ const MovieListing = () => {
             <div className='movie-list'>
                 <h2>Movies</h2>
                 <div className='movie-container'>
-                    {renderMovies}
+                    <Slider {...settings}>
+                        {renderMovies}
+                    </Slider>
                 </div>
             </div>
             <div className='show-list'>
